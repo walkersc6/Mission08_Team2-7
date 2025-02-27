8a483d4172ea90b240c29bf5c47771523fed1b5c
Sarah Walker <sarahsweetpie6@gmail.com>
2025-02-27 14:50:32 -0700

    Created database, interface, and instances of the interface with the ability to save changes to database made with CRUD

---
+++ Mission08_Team2-5/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Team2_5.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int CategoryId { get; set; } //required
+        public string CategoryName { get; set; } // required
+    }
+}
